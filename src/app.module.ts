d5c61f59f332f3fe0ca020939aa37278c0b6bed6
thang0401 <khongbietthang0@gmail.com>
2025-03-03 15:45:15 +0700

    update first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TransactionsModule } from './transactions/transactions.module';
 import { TransactionService } from './transaction/transaction.service';
+import { TransactionModule } from './transaction/transaction.module';
+import { TransactionService } from './transaction/transaction.service';
 
 @Module({
-  imports: [TransactionsModule],
+  imports: [TransactionsModule, TransactionModule],
   controllers: [AppController],
   providers: [AppService, TransactionService],
 })
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -2,10 +2,10 @@ import { Controller, Get } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 @Controller('transactions')
 export class TransactionsController {
-    constructor(private readonly transactionService: TransactionsService){}
+    constructor(private readonly transactionsService: TransactionsService){}
     @Get('ethereum')
     async getEthTransaction(){
-        const transactions = await this.transactionService.getETHTransaction();
+        const transactions = await this.transactionsService.getETHTransaction();
         return transactions;
     } 
 
